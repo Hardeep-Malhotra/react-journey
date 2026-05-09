e176bffffc8cf6c005f7f32a74798c84cbc496e3
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-05-09 13:59:55 +0530

    feat: initialize project with useEffect hook example
    - Add package.json with dependencies and scripts for development
    - Include Vite configuration for React
    - Create main application structure with App component
    - Implement SERVER component for connecting to a chat server
    - Add chat connection logic with createConnection function
    - Create APP component to listen to global pointer events
    - Add styles for the application in App.css and index.css
    - Include SVG assets for branding
    - Set up basic HTML structure with index.html

---
+++ 22_useEffect_Hook/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SERVER from "./Examples of connecting to an external system/SERVER";
+export const App = () => {
+  return (
+    <div>
+      <SERVER />
+    </div>
+  );
+};
+
+export default App;
---
+++ 22_useEffect_Hook/src/Examples of connecting to an external system/Connecting to a chat server/SERVER.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import { createConnection } from "./chat";
+
+function ChatRoom({ roomId }) {
+  const [serverUrl, setServerUrl] = useState("https://localhost:1234");
+
+  useEffect(() => {
+    const connection = createConnection(serverUrl, roomId);
+    connection.connect();
+
+    return () => {
+      connection.disconnect();
+    };
+  }, [serverUrl, roomId]);
+
+  return (
+    <>
+      <label htmlFor="">
+        Server URL:{""}
+        <input
+          type="text"
+          value={serverUrl}
+          onChange={(e) => setServerUrl(e.target.value)}
+        />
+      </label>
+
+      <h1>Welcome to the {roomId} room!</h1>
+    </>
+  );
+}
+
+const SERVER = () => {
+  const [roomId, setRoomId] = useState("general");
+  const [show, setShow] = useState(false);
+  return (
+    <div>
+      <label htmlFor="">
+        Chose the chat room{""}
+        <select value={roomId} onChange={(e) => setRoomId(e.target.value)}>
+          <option value="general">general</option>
+          <option value="travel">travel</option>
+          <option value="music">music</option>
+        </select>
+      </label>
+
+      <button onClick={() => setShow(!show)}>
+        {show ? "Leave the chat room" : "Join the chat room"}
+      </button>
+      {show && <hr />}
+      {show && <ChatRoom roomId={roomId} />}
+    </div>
+  );
+};
+
+export default SERVER;
