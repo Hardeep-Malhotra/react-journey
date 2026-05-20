d59729f46fc0af7def90336ab5fb3de6c5559569
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-05-20 06:37:07 +0530

    feat: initialize Todo app with Context API and Local Storage
    - Add package.json for project dependencies and scripts
    - Include Vite logo SVG in public directory
    - Create main App component with Tailwind CSS styling
    - Add React logo SVG in assets directory
    - Set up global CSS file to import Tailwind CSS
    - Implement main entry point for React application
    - Configure Vite with React and Tailwind CSS plugins

---
+++ 28_Todo_ContextAPI_With_LocalStorage/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="  font-bold text-4xl  text-orange-700 hover:underline ">
+        Hy , Tailwind Css is Working...
+      </h1>
+    </div>
+  );
+}
+
+export default App;
