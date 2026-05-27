cd288dc2046c0f02bd44a1f7e1d0d9c84db690b5
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-05-27 11:52:10 +0530

    feat: add CollectionReducer to the store for managing collection state

--- 32_Redux_Project/src/app/store.js
+++ 32_Redux_Project/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import SearchReducer from "../features/SearchSlice";
+import CollectionReducer from "../features/CollectionSlice";
 const Store = configureStore({
   reducer: {
     search: SearchReducer,
+    collection: CollectionReducer,
   },
 });
 
