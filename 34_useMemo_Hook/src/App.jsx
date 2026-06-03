5c68b1a9de6491ee13fd046b09460a6564abbd57
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-06-03 18:22:51 +0530

    feat: Implement useMemo Hook for optimized performance in App component

--- 34_useMemo_Hook/src/App.jsx
+++ 34_useMemo_Hook/src/App.jsx
@@ -1,11 +1,42 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
 
-  return <></>;
+  function expensiveTask(num) {
+    console.log("Inside Expensive Task.....!");
+    for (let i = 0; i <= 100000000; i++) {
+      return num * 2;
+    }
+  }
+
+  let doubleValue = useMemo(() => expensiveTask(input), [input]);
+
+  return (
+    <>
+      <h1>Learn useMemo Hook</h1>
+      Count : {count}
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increment
+      </button>
+      <input
+        type="number"
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+        placeholder="Enter Any Number"
+      />
+      Output is : {doubleValue}
+    </>
+  );
 }
 
 export default App;
