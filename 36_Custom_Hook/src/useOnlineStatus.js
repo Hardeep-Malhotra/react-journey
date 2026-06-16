16d4c09141c346744c6342c3594978df5b5f6466
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-06-16 23:07:03 +0530

    feat: implement custom hook for online status detection and update App component

---
+++ 36_Custom_Hook/src/useOnlineStatus.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+function useOnlineStatus() {
+  const [isOnline, setIsOnline] = useState(true);
+
+  useEffect(() => {
+    function handleOnline() {
+      setIsOnline(true);
+    }
+    function handleOffline() {
+      setIsOnline(false);
+    }
+
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    return () => {
+      window.addEventListener("online", handleOnline);
+      window.addEventListener("offline", handleOffline);
+    };
+  }, []);
+  return isOnline;
+}
+
+export default useOnlineStatus;
